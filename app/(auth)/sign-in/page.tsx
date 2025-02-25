91ebb77218f32a8d7e082ef72530b95300c50226
jahangir2002 <jahangirshaikh553@gmail.com>
2025-02-25 11:29:13 +0530

    add sign-up and sign-in pages; include SVG icons for branding and functionality

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <div>
+      <h2>Sign In</h2>
+      <p>Please enter your credentials to log in.</p>
+    </div>
+  );
+};
+
+export default SignIn;
