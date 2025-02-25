1b00dd926105d315f3717bc9878601a040472fe9
jahangir2002 <jahangirshaikh553@gmail.com>
2025-02-25 12:58:51 +0530

    implement utility functions and update authentication pages; integrate new UI components and enhance TailwindCSS configuration

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,12 +1,6 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
-const SignIn = () => {
-  return (
-    <div>
-      <h2>Sign In</h2>
-      <p>Please enter your credentials to log in.</p>
-    </div>
-  );
-};
+const SignIn = () => <AuthForm type="sign-in" />;
 
 export default SignIn;
