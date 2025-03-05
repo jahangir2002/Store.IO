e9ecc682a32c433d47029f188a17abe42b8b7421
jahangir2002 <jahangirshaikh553@gmail.com>
2025-03-05 12:05:02 +0530

    Add Radix UI Alert Dialog and Input OTP for enhanced authentication flow make the OTPModel

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,7 +18,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { createAccount, signInUser } from "@/lib/actions/users.actions";
-// import OtpModal from "@/components/OTPModal";
+import OTPModal from "./OTPModal";
 
 type FormType = "sign-in" | "sign-up";
 
@@ -157,9 +157,9 @@ const AuthForm = ({ type }: { type: FormType }) => {
         </form>
       </Form>
 
-      {/* {accountId && (
-        <OtpModal email={form.getValues("email")} accountId={accountId} />
-      )} */}
+      {true && (
+        <OTPModal email={form.getValues("email")} accountId={accountId} />
+      )}
     </>
   );
 };
--- lib/actions/users.actions.ts
+++ lib/actions/users.actions.ts
@@ -14,7 +14,7 @@ const getUserByEmail = async (email: string) => {
   const result = await databases.listDocuments(
     appwriteConfig.databaseId,
     appwriteConfig.usersCollectionId,
-    [Query.equal("email", [email])],
+    [Query.equal("email", [email])]
   );
 
   return result.total > 0 ? result.documents[0] : null;
@@ -59,9 +59,10 @@ export const createAccount = async ({
       {
         fullName,
         email,
-        avatar: "https://icons.veryicon.com/png/o/miscellaneous/standard/avatar-15.png",
+        avatar:
+          "https://icons.veryicon.com/png/o/miscellaneous/standard/avatar-15.png",
         accountId,
-      },
+      }
     );
   }
 
@@ -102,7 +103,7 @@ export const getCurrentUser = async () => {
     const user = await databases.listDocuments(
       appwriteConfig.databaseId,
       appwriteConfig.usersCollectionId,
-      [Query.equal("accountId", result.$id)],
+      [Query.equal("accountId", result.$id)]
     );
 
     if (user.total <= 0) return null;
