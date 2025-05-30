149346e5962238f420583b8aeef2d6baa41609f7
jahangir2002 <jahangirshaikh553@gmail.com>
2025-05-30 16:46:14 +0530

    changes

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -8,8 +8,6 @@ import { constructFileUrl, getFileType, parseStringify } from "../utils";
 import { url } from "inspector";
 import { error } from "console";
 import { revalidatePath } from "next/cache";
-import { getCurrentUser } from "./users.actions";
-import { create } from "domain";
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
@@ -60,62 +58,5 @@ export const uploadFile = async ({
     return parseStringify(newFile);
   } catch (error) {
     handleError(error, "Failed to Upload file");
-  }
-};
-
-const createQueries = (
-  currentUser: Models.Document,
-  types: string[],
-  searchText: string,
-  sort: string,
-  limit?: number
-) => {
-  const queries = [
-    Query.or([
-      Query.equal("owner", [currentUser.$id]),
-      Query.contains("users", [currentUser.email]),
-    ]),
-  ];
-
-  if (types.length > 0) queries.push(Query.equal("type", types));
-  if (searchText) queries.push(Query.contains("name", searchText));
-  if (limit) queries.push(Query.limit(limit));
-
-  if (sort) {
-    const [sortBy, orderBy] = sort.split("-");
-
-    queries.push(
-      orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy)
-    );
-  }
-
-  return queries;
-};
-
-export const getFiles = async ({
-  types = [],
-  searchText = "",
-  sort = "$createdAt-desc",
-  limit,
-}: GetFilesProps) => {
-  const { databases } = await createAdminClient();
-
-  try {
-    const currentUser = await getCurrentUser();
-
-    if (!currentUser) throw new Error("User not found");
-
-    const queries = createQueries(currentUser, types, searchText, sort, limit);
-
-    const files = await databases.listDocuments(
-      appwriteConfig.databaseId,
-      appwriteConfig.filesCollectionId,
-      queries
-    );
-
-    console.log({ files });
-    return parseStringify(files);
-  } catch (error) {
-    handleError(error, "Failed to get files");
   }
 };
